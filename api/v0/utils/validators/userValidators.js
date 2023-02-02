d1650761f5e09f74515545415d0cf58e86a809b6
iam-phenomenal <ayandiranibrahim@outlook.com>
2023-02-02 10:17:12 +0100

    Added optional method to request body

--- api/v0/utils/validators/userValidators.js
+++ api/v0/utils/validators/userValidators.js
@@ -3,26 +3,10 @@ const User = require("../../models/User")
 
 //User update request validators
 const updateValidator = [
-    //Verify userid is valid
-    param("user_id").custom(async (value)=>{
-        //Find user by id
-        try{
-            const user = await User.findById(value);
-            if(!user){
-                throw new Error("Invalid user id");
-            }else{
-                return true;
-            }
-        }catch(err){
-            throw new Error("Invalid user id");
-        }
-    }),
     //Ensure username can't be changed
-    body("username").custom((value)=>{
-        throw new Error("Username is immutable");
-    }),
+    body("username").not().exists().withMessage("Username is immutable"),
     //Ensure password is strong
-    body("user_password").isStrongPassword({
+    body("user_password").optional({checkFalsy: true}).isStrongPassword({
         minLowercase: 1,
         minLength: 8,
         minSymbols: 1,
@@ -31,7 +15,7 @@ const updateValidator = [
     }).withMessage("Minimum of 8 characters with"+
         "Lowercase, uppercase, symbol and number"),
     //Ensure email is valid and doesn't exist
-    body("email").isEmail().withMessage("Please enter a valid email!").custom(async(value)=>{
+    body("email").optional({checkFalsy: true}).isEmail().withMessage("Please enter a valid email!").custom(async(value)=>{
         try{
             const user = await User.findOne({email: value});
             throw new Error("Email already exists");
